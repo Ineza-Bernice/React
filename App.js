b572fd78689eb589320422cb4cb97b73e84fcda3
Ineza-Bernice <122214795+Ineza-Bernice@users.noreply.github.com>
2023-05-21 08:39:18 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CarSellForm from "./component/CarSellForm";
+
+function App() {
+  return (
+    <div>
+      <CarSellForm />
+    </div>
+  );
+}
+
+export default App;
